bc14fdd6d66f93e33597a55d5b43d9538a2bc633
SolxSaj <solracsajor22@gmail.com>
2022-07-05 12:12:28 -0500

    Creación de modelos

---
+++ app/Models/CriteriosEspecificos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CriteriosEspecificos extends Model
+{
+    use HasFactory;
+}
